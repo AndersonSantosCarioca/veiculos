45c224f9cb319cba51192f2d055a95a3990bc2b3
Anderson Santos <cariocacapoeira3497@gmail.com>
2023-09-18 07:45:28 -0300

    veiculo subindo

---
+++ veiculos/src/abstracts/Veiculo.java
@@ -0,0 +1,31 @@
+public abstract class Veiculo {
+    private String marca;
+    private String modelo;
+
+
+    public Veiculo(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public abstract void acelerar();
+    public abstract void frear();
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    
+}
